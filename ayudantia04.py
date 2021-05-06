6b85b72527f531d432e8cedc1af1a78a61ba4256
gabiacuna <gabriela.acuna@usm.cl>
2021-05-05 20:17:26 -0400

    ayu 4

--- ayudantia04.py
+++ ayudantia04.py
@@ -47,3 +47,59 @@ while flag:
     if cont >= 10:
         flag = False
 
+
+'''
+Ejemplos:
+'''
+
+
+# pot = 0
+# res = 0
+
+# while pot < 10:
+#     res += n**pot
+#     pot += 1
+
+# print(res)
+
+# 5! = 1*2*3*4*5
+
+#Factorial: 
+
+# res = 1
+
+# while n > 0:
+#     res *= n
+#     n -= 1
+
+# print(res)
+
+#Conteo:
+# n = int(input('ingrese un numero:\t'))
+# cont = 0
+
+# while n > 0:
+#     n -= 1
+
+#     m = int(input('num:\t'))
+
+#     if m % 3 == 0:
+#         cont += 1
+
+# print(cont, 'num eran multiplos de 3')
+
+
+#max/min
+
+n = int(input('ingrese un numero:\t'))
+
+mayor = 9999
+
+while n > 0:
+    n -= 1
+    m = int(input('num:\t'))
+
+    if mayor > m:
+        mayor = m
+
+print('el mayor es:\t', mayor)
